425a8fdc312a59e5dc5d54b1764cbed68b9411e8
Takahiro5286 <119845672+Takahiro5286@users.noreply.github.com>
2023-12-11 12:20:33 +0900

    first commit

---
+++ manyBlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  manyBlocksApp.swift
+//  manyBlocks
+//
+//  Created by user on 2023/12/07.
+//
+
+import SwiftUI
+
+@main
+struct manyBlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
