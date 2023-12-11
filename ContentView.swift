e1eb68ede37e7e2c6ab5ce070c0a51722bbc24f3
Takahiro5286 <119845672+Takahiro5286@users.noreply.github.com>
2023-12-11 12:23:29 +0900

    third commit

---
+++ ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  manyBlocks
+//
+//  Created by user on 2023/12/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        blockSlide()
+    }
+}
+
+#Preview {
+    ContentView()
+}
